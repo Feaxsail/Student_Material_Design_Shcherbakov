5bed529e3ccac88d5aa19bcff658831fbf671ef8
whypikmi <ttelatnikova1@yandex.ru>
2026-03-10 14:26:56 +0300

    Step 5: Basic UI completed

--- app/src/main/java/com/shcherbakov/shcherbakov_material_design/MainActivity.kt
+++ app/src/main/java/com/shcherbakov/shcherbakov_material_design/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.shcherbakov.shcherbakov_material_design.data.Student
+import com.shcherbakov.shcherbakov_material_design.data.students
 import com.shcherbakov.shcherbakov_material_design.ui.theme.Shcherbakov_Material_DesignTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +27,67 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Shcherbakov_Material_DesignTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    StudentApp()
                 }
             }
         }
     }
+}
+
+@Composable
+fun StudentIcon(@DrawableRes studentIcon: Int) {
+    Image(
+        painter = painterResource(id = studentIcon),
+        contentDescription = null,
+        modifier = Modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(R.dimen.padding_small))
+    )
+}
+
+@Composable
+fun StudentInformation(@StringRes studentName: Int, studentAge: Int) {
+    Column {
+        Text(
+            text = stringResource(studentName),
+            style = MaterialTheme.typography.displayMedium
+        )
+        Text(
+            text = stringResource(R.string.years_old, studentAge),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Composable
+fun StudentItem(student: Student) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_medium))
+    ) {
+        StudentIcon(student.imageResourceId)
+        StudentInformation(student.name, student.age)
+    }
+}
+
+@Composable
+fun StudentApp() {
+    LazyColumn {
+        items(students) { student ->
+            StudentItem(student)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StudentPreview() {
+    Shcherbakov_Material_DesignTheme(darkTheme = false) {
+        StudentApp()
+    }
+}
